a95404803f434c6dd507c443e15f3d262317a94f
Evelina Stonyte <stonyteevelina@gmail.com>
2023-04-08 18:06:27 +0300

    corrected players.py code

--- playersdata.py
+++ playersdata.py
@@ -1,18 +1,22 @@
 import requests
 import json
+import time
 
+file = open('nbaplayers.txt', 'w')
 page_number = 0
 while True:
     page_number += 1
-    query = {'page': '1', 'per_page': '25'}
+    query = {'page': str(page_number), 'per_page': '25'}
 
-    #Reads API and checks if there're no errors
     try:
         response_API = requests.get('https://www.balldontlie.io/api/v1/players', timeout=50, params=query)
+        if response_API != 200:
+            time.sleep(20)
         response_API.raise_for_status()
-        # Code here will only run if the request is successful
     except requests.exceptions.HTTPError as errh:
         print(errh)
+    except requests.exceptions.TooManyRedirects:
+        print()
     except requests.exceptions.ConnectionError as errc:
         print(errc)
     except requests.exceptions.Timeout as errt:
@@ -20,15 +24,9 @@ while True:
     except requests.exceptions.RequestException as err:
         print(err)
 
-
     #Reads API and opens json:
     playersData = json.loads(response_API.text)
-
-
-    #Uploading data to file:
-
     #Data about players:
-    file = open('nbaplayers.txt', 'w')
     file.write('Name,Surname,Height(inches),Height(feet)\n')
     allPlayers = len(playersData['data'])
     complete_data = ''
@@ -47,6 +45,7 @@ while True:
         if item['height_feet'] is not None:
             complete_data += str(item['height_feet'])
         complete_data += '\n'
-    file.write(complete_data)
-    break
+        file.write(complete_data)
+    if complete_data is not True:
+        break
 file.close()

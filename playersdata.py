155abb7a90c77c948c05f0d6cbf0a7dc34ba9cb3
Evelina Stonyte <stonyteevelina@gmail.com>
2023-04-11 07:37:11 +0300

    Update games

--- playersdata.py
+++ playersdata.py
@@ -1,24 +1,23 @@
 import requests
 import json
 from datetime import datetime, timedelta
-
+import time
 
 #Gets yesterdays date 
 yesterday_datetime = datetime.now() - timedelta(days=1)
 yesterday_date = yesterday_datetime.strftime('%Y-%m-%d')
 
 file = open('%s.txt' % yesterday_date, 'w')
-
+page_number = 0
 while True:
-    i = 0
-    i = i + 1
-    query = {'page': str(i), 'per_page':'25', 'search': '', 'dates': str(yesterday_date)}
+    page_number += 1
+    query = {'page': str(page_number), 'per_page': '25', 'dates': str(yesterday_date)}
 
-    #Reads API and checks if there're no errors
     try:
         response_API = requests.get('https://www.balldontlie.io/api/v1/stats', timeout=50, params=query)
+        if response_API != 200:
+            time.sleep(20)
         response_API.raise_for_status()
-        # Code here will only run if the request is successful
     except requests.exceptions.HTTPError as errh:
         print(errh)
     except requests.exceptions.ConnectionError as errc:
@@ -28,44 +27,30 @@ while True:
     except requests.exceptions.RequestException as err:
         print(err)
 
-
     #Reads API and opens json:
     playersData = json.loads(response_API.text)
-
     #Data about players:
     allPlayers = len(playersData['data'])
+    complete_data = ''
 
     for player in range(0, allPlayers):
         item = playersData['data'][player]
         if item['player']['first_name'] != None:
-            file.write('Name: ')
-            file.writelines(str(item['player']['first_name']))
+            complete_data += str(item['player']['first_name'])
+        complete_data += ','
         if item['player']['last_name'] != None:
-            file.write(' Surame: ')
-            file.writelines(str(item['player']['last_name']))
+            complete_data += str(item['player']['last_name'])
+        complete_data += ','
         if item['pts'] != None:
-            file.write(' Points: ')
-            file.writelines(str(item['pts']))
-        if  item['reb'] != None:
-            file.write(' Rebound: ')
-            file.writelines(str(item['reb']))
-        if  item['stl'] != None:
-            file.write(' Steals: ')
-            file.writelines(str(item['stl']))
-        file.write('\n')
-
+            complete_data += str(item['pts'])      
+        complete_data += ','
+        if item['reb'] != None:
+            complete_data += str(item['reb'])
+        complete_data += ','
+        if item['stl'] != None:
+            complete_data += str(item['stl'])
+        complete_data += '\n'
+        file.write(complete_data)
+    if complete_data is not True:
+        break
 file.close()
-        
-   
-
-
-
-
-
-
-   
-
-
-   
-
-
